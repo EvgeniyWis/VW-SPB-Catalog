79af477bad98657e7fd1d34f90b7e81691f7140d
Evgeniy <KworkEvgeniy@yandex.ru>
2024-04-08 21:48:35 +0300

    Завершение работы над адаптивностью страницы.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,12 @@ const navbar__adaptive_menu = document.getElementById("navbar__adaptive_menu");
 const navbar__adaptive_menu_cross = document.getElementById("navbar__adaptive_menu_cross");
 const filters__range_slider_min__number = document.getElementById("filters__range_slider_min__number");
 const filters__range_slider_max__number = document.getElementById("filters__range_slider_max__number");
+const catalog_filters = document.getElementById("catalog_filters");
+const filters = document.getElementById("filters");
+const catalog_wrapper = document.getElementById("catalog_wrapper");
+const filters__header__close = document.getElementById("filters__header__close");
+const filters__header__delete = document.getElementById("filters__header__delete");
+const filters__show_items = document.getElementById("filters__show_items");
 
 /* Открытие и закрытие бургер меню */
 navbar__adaptive_menuBurger.addEventListener("click", () => {
@@ -53,3 +59,34 @@ for (let item of filters__colors__items) {
         item.classList.toggle("filters__colors--item--active");
     })
 }
+
+/* Открытие адаптивного блока "Фильтры" */
+catalog_filters.addEventListener("click", () => {
+    if (filters.style.display == "none" || !filters.style.display) {
+        filters.style.display = "flex";
+        catalog_wrapper.classList.add("hidden");
+        filters__show_items.classList.remove("hidden");
+    }
+    else {
+        filters.style.display = "none";
+        catalog_wrapper.classList.remove("hidden");
+    }
+});
+
+/* Закрытие адаптивного блока "Фильтры" */
+filters__header__close.addEventListener("click", () => {
+    if (filters.style.display == "flex") {
+        filters.style.display = "none";
+        catalog_wrapper.classList.remove("hidden");
+    }
+    else {
+        filters.style.display = "flex";
+        catalog_wrapper.classList.add("hidden");
+    }
+});
+
+/* Открытие товаров */
+filters__show_items.addEventListener("click", () => {
+    filters__show_items.classList.add("hidden");
+    catalog_wrapper.classList.remove("hidden");
+})
