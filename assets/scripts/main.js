d509ac34c494bf44930c95ec9410446700adeec4
Evgeniy <KworkEvgeniy@yandex.ru>
2024-04-08 15:14:49 +0300

    Добавление функционала изменения данных в input range.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,8 @@
 const navbar__adaptive_menuBurger = document.getElementById("navbar__adaptive_menuBurger");
 const navbar__adaptive_menu = document.getElementById("navbar__adaptive_menu");
 const navbar__adaptive_menu_cross = document.getElementById("navbar__adaptive_menu_cross");
-
+const filters__range_slider_min__number = document.getElementById("filters__range_slider_min__number");
+const filters__range_slider_max__number = document.getElementById("filters__range_slider_max__number");
 
 /* Открытие и закрытие бургер меню */
 navbar__adaptive_menuBurger.addEventListener("click", () => {
@@ -16,10 +17,21 @@ navbar__adaptive_menu_cross.addEventListener("click", () => {
 })
 
 /* Функционал слайдера в блоке "Цена" */
-
 $(".filters__range_slider_input").ionRangeSlider({
     type: "double",
     min: 3650000,
     max: 6745000,
+});
 
+document.addEventListener("DOMContentLoaded", () => {
+    const irs_from = document.querySelector(".irs-from");
+    const irs_to = document.querySelector(".irs-to");
+
+    irs_from.addEventListener('DOMSubtreeModified', function () {
+        filters__range_slider_min__number.textContent = irs_from.textContent;
+    });
+
+    irs_to.addEventListener('DOMSubtreeModified', function () {
+        filters__range_slider_max__number.textContent = irs_to.textContent;
+    });
 });
